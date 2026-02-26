3bb0d400b0dab7a2da0331622f8d4cc92a212a50
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-02-26 06:54:29 +0530

    feat: code reference component to show the code related to the file with react-syntax-highlighting

---
+++ src/app/(protected)/dashboard/code-references.tsx
@@ -0,0 +1,63 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+import React from "react";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  fileReferences: {
+    fileName: string;
+    sourceCode: string;
+    summary: string;
+  }[];
+};
+
+export const CodeReferences = ({ fileReferences }: Props) => {
+  const [tab, setTab] = React.useState(fileReferences[0]?.fileName);
+  return (
+    <div className="w-full">
+      <Tabs value={tab} onValueChange={setTab}>
+        <div className="flex gap-2 overflow-scroll rounded-md bg-gray-200 p-1">
+          {fileReferences.map((file, index) => (
+            <button
+              key={file.fileName}
+              onClick={() => setTab(file.fileName)}
+              className={cn(
+                "text-muted-foreground hover:bg-muted rounded-md px-3 py-1.5 text-sm font-medium whitespace-nowrap transition-colors",
+                {
+                  "bg-primary text-primary-foreground": tab === file.fileName,
+                },
+              )}
+            >
+              {file.fileName}
+            </button>
+          ))}
+        </div>
+        {fileReferences.map((file) => (
+          <TabsContent
+            key={file.fileName}
+            value={file.fileName}
+            className="max-h-[40vh] max-w-7xl overflow-scroll rounded-md"
+          >
+            <SyntaxHighlighter
+              language="typescript"
+              style={vscDarkPlus}
+              showLineNumbers
+              wrapLines
+            >
+              {file.sourceCode}
+            </SyntaxHighlighter>
+          </TabsContent>
+        ))}
+      </Tabs>
+    </div>
+  );
+};
