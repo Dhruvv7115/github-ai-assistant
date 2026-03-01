5c5cb2a4ee39651204f60338701649529d1bdafd
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-01 06:19:07 +0530

    feat: new archive project button to archive projects

---
+++ src/app/(protected)/dashboard/archive-button.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useProjects } from "@/hooks/use-projects";
+import { useRefetch } from "@/hooks/use-refetch";
+import { api } from "@/trpc/react";
+import React from "react";
+import { toast } from "sonner";
+
+const ArchiveButton = () => {
+  const { projectId } = useProjects();
+  const archiveProject = api.project.archiveProject.useMutation();
+  const refetch = useRefetch();
+  return (
+    <Button
+      variant="outline"
+      disabled={archiveProject.isPending}
+      size="sm"
+      onClick={() => {
+        const confirm = window.confirm(
+          "Are you sure you want to archive this project?",
+        );
+        if (!confirm) return;
+        archiveProject.mutate(
+          {
+            projectId,
+          },
+          {
+            onSuccess: () => {
+              toast.success("Project archived successfully!");
+              refetch();
+            },
+            onError: () => {
+              toast.error("Failed to archive this project!");
+            },
+          },
+        );
+      }}
+    >
+      {" "}
+      Archive
+    </Button>
+  );
+};
+
+export default ArchiveButton;
