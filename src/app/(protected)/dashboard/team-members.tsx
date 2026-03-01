49766de8e450dd25bab51218a0c58de584a79118
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-01 06:20:03 +0530

    feat: team members component to show members on a single project

---
+++ src/app/(protected)/dashboard/team-members.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useProjects } from "@/hooks/use-projects";
+import { api } from "@/trpc/react";
+import React from "react";
+
+const TeamMembers = () => {
+  const { projectId } = useProjects();
+  const { data: members } = api.project.getTeamMembers.useQuery({
+    projectId,
+  });
+  return (
+    <div className="flex items-center gap-2">
+      {members?.slice(0, 3)?.map((member) => (
+        <img
+          src={member.user.imageUrl || ""}
+          alt={member.user.firstName || ""}
+          height={30}
+          width={30}
+          className="rounded-full"
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TeamMembers;
