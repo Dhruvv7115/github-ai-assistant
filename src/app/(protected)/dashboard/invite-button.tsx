646d52b3c77547e5343631ad97e28fcf86a389f6
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-01 06:19:34 +0530

    feat: new invite button to add new members to the same project

---
+++ src/app/(protected)/dashboard/invite-button.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useProjects } from "@/hooks/use-projects";
+import React from "react";
+import { toast } from "sonner";
+
+const InviteButton = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const { projectId } = useProjects();
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite Team Members</DialogTitle>
+          </DialogHeader>
+          <p className="text-sm text-neutral-500">
+            Ask them to copy and paste this link
+          </p>
+          <Input
+            className="mt-4"
+            readOnly
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${window.location.origin}/join/${projectId}`,
+              );
+              toast.success("Link copied to clipboard");
+            }}
+            value={`${window.location.origin}/join/${projectId}`}
+          />
+        </DialogContent>
+      </Dialog>
+      <Button variant="ghost" size="sm" onClick={() => setOpen(true)}>
+        Invite Members
+      </Button>
+    </>
+  );
+};
+
+export default InviteButton;
